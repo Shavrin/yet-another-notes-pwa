ec5520f4f23f44be52964c41397bbee2201abde1
Kacper <kacper.olek@gmail.com>
2019-05-23 01:00:48 +0200

    Progress

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = function (){
+    return (
+            <h1 className="titleBar">
+                <a href="">
+                Yet Another Notes PWA
+                </a>
+            </h1>
+        );
+}
+
+export default Header;
